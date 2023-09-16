10d2a10c17cfda9d3786b93956779401579966d2
Fidel <fidelbello1997@gmail.com>
2023-09-16 13:58:38 -0400

    added usd format function

--- src/helpers/formatToTwoDecimalPlaces.ts
+++ src/helpers/formatToTwoDecimalPlaces.ts
@@ -1,3 +1,7 @@
 export function formatToTwoDecimalPlaces(num: number) {
-  return num.toFixed(2);
+  return num.toFixed(2) as unknown as number;
+}
+
+export function formatToUSD(num: number) {
+  return `$${num.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}`;
 }
