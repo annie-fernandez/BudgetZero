6fd84888cd6eadfba95211f103250f04980c4a27
Anthony Fernandez <anthonyfdez23@gmail.com>
2023-09-17 07:50:08 -0400

    Make icon link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,49 +12,65 @@ import SettingsDrawer from "../SettingsDrawer/SettingsDrawer";
 import { Settings } from "react-feather";
 import { IconSearch } from "@tabler/icons-react";
 import { spotlight } from "@mantine/spotlight";
-
+import { Link } from "react-router-dom";
 
 const useStyles = createStyles(() => ({
   logo: {
     height: 50,
-  }
+  },
 }));
 
 const Header = () => {
   const { classes } = useStyles();
+  const { theme } = useStyles();
 
   const { setApp } = useGlobalStore();
 
-
   return (
     <MantineHeader height={{ base: 50, md: 70 }} p="md">
-  <Flex style={{ height: "100%" }} align={"center"} justify={"space-between"}>
-    <Flex align={"center"}>
-      <img className={classes.logo} src="/logo.png" />
-      <Text ml={-10}>BudgetZero</Text>
-    </Flex>
-    <Flex align={"center"}>
-      <Center>
-        <Input
-          style={{ cursor: "pointer"}}
-          icon={<IconSearch size={10}/>}
-          placeholder="Search"
-          size={"xs"}
-          readOnly
-          onClick={() => spotlight.open()}
-        />
-      </Center>
-      <ActionIcon
-        onClick={() => {
-          setApp({ isSettingsDrawerOpen: true });
-        }}
+      <Flex
+        style={{ height: "100%" }}
+        align={"center"}
+        justify={"space-between"}
       >
-        <Settings size={16} style={{ marginLeft: 4 }} />
-      </ActionIcon>
-    </Flex>
-  </Flex>
-  <SettingsDrawer />
-</MantineHeader>  
+        <Link to="/app">
+          <Flex align={"center"}>
+            <img className={classes.logo} src="/logo.png" />
+            <Text
+              style={{
+                textDecoration: "none",
+                color: theme.colorScheme === "dark" ? theme.white : theme.black,
+              }}
+              underline={false}
+              ml={-10}
+            >
+              BudgetZero
+            </Text>
+          </Flex>
+        </Link>
+
+        <Flex align={"center"}>
+          <Center>
+            <Input
+              style={{ cursor: "pointer" }}
+              icon={<IconSearch size={10} />}
+              placeholder="Search"
+              size={"xs"}
+              readOnly
+              onClick={() => spotlight.open()}
+            />
+          </Center>
+          <ActionIcon
+            onClick={() => {
+              setApp({ isSettingsDrawerOpen: true });
+            }}
+          >
+            <Settings size={16} style={{ marginLeft: 4 }} />
+          </ActionIcon>
+        </Flex>
+      </Flex>
+      <SettingsDrawer />
+    </MantineHeader>
   );
 };
 
