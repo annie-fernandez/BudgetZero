9c08756aab56390119a2aba55cd62d9b4c2930e2
Anthony Fernandez <anthonyfdez23@gmail.com>
2023-09-17 06:28:56 -0400

    Massive bug fix

--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -1,9 +1,12 @@
+import useLoadUserData from "../../hooks/useLoadUserData";
 import { Faq } from "./sections/faq/Faq";
 import { Features } from "./sections/features/Features";
 import { Jumbo } from "./sections/jumbo/Jumbo";
 import LandingPageHeader from "./sections/landingPageHeader/LandingPageHeader";
 
 const LandingPage = (): JSX.Element => {
+  useLoadUserData();
+
   return (
     <>
       <LandingPageHeader />
