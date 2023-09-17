9ef888a5e2ad45dfa97f73afbf15831ca43fc85f
Fidel <fidelbello1997@gmail.com>
2023-09-17 06:43:17 -0400

    added: Input for searches

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,50 +1,60 @@
 import {
   ActionIcon,
+  Center,
   Flex,
+  Input,
   Header as MantineHeader,
-  MediaQuery,
   Text,
   createStyles,
 } from "@mantine/core";
 import useGlobalStore from "../../store/useGlobalStore";
 import SettingsDrawer from "../SettingsDrawer/SettingsDrawer";
 import { Settings } from "react-feather";
+import { IconSearch } from "@tabler/icons-react";
+import { spotlight } from "@mantine/spotlight";
+
 
 const useStyles = createStyles(() => ({
   logo: {
     height: 50,
-  },
+  }
 }));
 
 const Header = () => {
-
   const { classes } = useStyles();
 
-  const {
-    setApp,
-  } = useGlobalStore();
+  const { setApp } = useGlobalStore();
+
 
   return (
     <MantineHeader height={{ base: 50, md: 70 }} p="md">
-      <SettingsDrawer />
-      <Flex style={{ height: "100%" }} align="center" justify="space-between">
-        <div>
-          <MediaQuery smallerThan={"sm"} styles={{ display: "none" }}>
-            <Flex align="center">
-              <img className={classes.logo} src="/logo.png" />
-              <Text ml={-10}>BudgetZero</Text>
-            </Flex>
-          </MediaQuery>
-        </div>
-        <ActionIcon
-          onClick={() => {
-            setApp({ isSettingsDrawerOpen: true });
-          }}
-        >
-          <Settings size={16} />
-        </ActionIcon>
-      </Flex>
-    </MantineHeader>
+  <Flex style={{ height: "100%" }} align={"center"} justify={"space-between"}>
+    <Flex align={"center"}>
+      <img className={classes.logo} src="/logo.png" />
+      <Text ml={-10}>BudgetZero</Text>
+    </Flex>
+    <Flex align={"center"}>
+      <Center>
+        <Input
+          style={{ cursor: "pointer"}}
+          icon={<IconSearch size={10}/>}
+          placeholder="Search"
+          size={"xs"}
+          readOnly
+          onClick={() => spotlight.open()}
+        />
+      </Center>
+      <ActionIcon
+        onClick={() => {
+          setApp({ isSettingsDrawerOpen: true });
+        }}
+      >
+        <Settings size={16} style={{ marginLeft: 4 }} />
+      </ActionIcon>
+    </Flex>
+  </Flex>
+  <SettingsDrawer />
+</MantineHeader>  
   );
 };
 
